23a756aba81e285439b151e31ad47de4420a2e6b
Nick Bristow <36126654+AtelierLibre@users.noreply.github.com>
2019-02-07 15:40:00 -0300

    Creating __init__.py

---
+++ osmuf/__init__.py
@@ -0,0 +1,8 @@
+################################################################################
+# Module: __init__.py
+# Description: OSMuf
+# License: MIT, see full license in LICENSE.txt
+# Web: https://github.com/AtelierLibre/osmuf
+################################################################################
+
+from .core import *
