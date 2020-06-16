751e47ebd4ba3959ff78f8f024616ea7b1d45dd3
Dag <dwastberg@gmail.com>
2020-06-16 13:22:12 +0200

    update save_and_show import

--- osmuf/plot.py
+++ osmuf/plot.py
@@ -26,7 +26,7 @@ from shapely import wkt
 from descartes import PolygonPatch
 
 from osmnx import settings
-from osmnx import save_and_show
+from osmnx.plot import _save_and_show as save_and_show
 from osmnx.utils import log
 from osmnx.utils import make_str
 
